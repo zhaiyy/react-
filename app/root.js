4b28f4734638670cd053a4cbddec4a453bf8d65e
zhaiyingying-bd <zhaiyingying-bd@ofo.com>
2018-05-22 20:59:16 +0800

    palyer

--- app/root.js
+++ app/root.js
@@ -3,11 +3,32 @@ import Header from './components/header'
 import Progress from './components/progress'
 
 class Root extends React.Component{
+    constructor(props) {
+        console.log(props)
+        super(props)
+        this.state = {progress: '-'}
+    };
+    componentDidMount() {
+        $('#player').jPlayer({
+            ready: function () {
+                $(this).jPlayer("setMedia", {
+                    mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
+                }).jPlayer('play');
+            },
+            supplied: 'mp3',
+            vmode: 'window'
+        })
+        $("#player").bind($.jPlayer.event.timeupdate, (e) => {
+            this.setState({
+                progress: Math.round(e.jPlayer.status.currentTime)
+            });
+        });
+    };
     render(){
         return (
             <div>
                 <Header></Header>
-                <Progress progress='2'></Progress>
+                <Progress progress={this.state.progress}></Progress>
             </div>
         );
     }
