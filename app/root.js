9600aa376f7b49776fd7c70c62602294d39ea4ac
zhaiyingying-bd <zhaiyingying-bd@ofo.com>
2018-05-23 10:10:40 +0800

    progress

--- app/root.js
+++ app/root.js
@@ -9,20 +9,24 @@ class Root extends React.Component{
         this.state = {progress: '-'}
     };
     componentDidMount() {
-        $('#player').jPlayer({
+        $("#player").jPlayer({
             ready: function () {
                 $(this).jPlayer("setMedia", {
                     mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
                 }).jPlayer('play');
             },
-            supplied: 'mp3',
-            vmode: 'window'
-        })
+            supplied: "mp3",
+            wmode: "window",
+            useStateClassSkin: true
+        });
         $("#player").bind($.jPlayer.event.timeupdate, (e) => {
             this.setState({
-                progress: Math.round(e.jPlayer.status.currentTime)
+                progress: e.jPlayer.status.currentPercentAbsolute
             });
         });
+    };
+    componentWillUnmount() {
+        $("#player").unbind($.jPlayer.event.timeupdate);
     };
     render(){
         return (
